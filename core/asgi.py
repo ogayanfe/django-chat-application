e05eb03222889c9e495ab51d3debab3d7babb34f
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-09-14 22:17:59 +0100

    Restyled the edit and signup links

--- core/asgi.py
+++ core/asgi.py
@@ -13,12 +13,16 @@ import chat.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
-    ),
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        )),
 })
