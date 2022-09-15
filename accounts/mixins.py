2b5606a35ade3e19ddf4937edf414ffea3fc962e
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2022-09-15 01:47:17 +0100

    Final Commit --- Maybe :)

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -2,10 +2,11 @@ from .models import UserProfile
 
 
 class CreateUserProfileInstanceIfUserIsAuthenticated:
-    # The function of this code is to ensure that every user gets and authmatically generated
+    # The function of this code is to ensure that every user gets and authmatically
+    # generates a userprofile before finishing the get request
     # user profile
     def get(self, *args):
         user = self.request.user if self.request.user.is_authenticated else None
-        if not UserProfile.objects.filter(user=user).exists():
+        if user and not UserProfile.objects.filter(user=user).exists():
             UserProfile.objects.create(user=user)
         return super().get(*args)
